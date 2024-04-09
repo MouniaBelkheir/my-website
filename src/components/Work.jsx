92b8b6e6d7fea9d3ce17d457b8eca62fe7914d07
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-04-09 08:05:54 +0100

    styled work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -33,8 +33,8 @@ const Work = () => {
 	return (
 		<div
 			id="work"
-			className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
-			<h1 className="text-4xl font-bold text-center">Work</h1>
+			className="max-w-[1040px] m-auto md:pl-20 p-4 py-16 text-gray-800">
+			<h1 className="text-4xl font-bold text-center ">Work</h1>
 			{data.map((item, index) => (
 				<WorkItem
 					key={index}
--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -1,26 +1,30 @@
 import PropTypes from "prop-types";
 
 const WorkItem = ({ year, title, duration, details }) => {
-  return (
-    <ol className="flex flex-col md:flex-row relative border-pink-800">
-      <li>
-        <div />
-        <p>
-          <span>{year}</span>
-          <span>{title}</span>
-          <span>{duration}</span>
-        </p>
-        <p>{details}</p>
-      </li>
-    </ol>
-  );
+	return (
+		<ol className="flex flex-col md:flex-row relative border-pink-800">
+			<li className="mb-10 ml-4 ">
+				<div className="absolute w-3 h-3 bg-pink-700 rounded-full mt-1.5 -left-1.5 border-white" />
+				<p className="flex flex-wrap gap-4 flex-row justify-start text-sm">
+					<span className="inline-block px-2 py-1 font-semibold text-white bg-pink-600/65 rounded-lg">
+						{year}
+					</span>
+					<span className="text-lg font-semibold text-pink-700">{title}</span>
+					<span className="my-1.5 text-sm leading-none text-stone-400">
+						{duration}
+					</span>
+				</p>
+				<p className="my-2 text-stone-500">{details}</p>
+			</li>
+		</ol>
+	);
 };
 
 WorkItem.propTypes = {
-  year: PropTypes.any,
-  title: PropTypes.string,
-  duration: PropTypes.array,
-  details: PropTypes.string,
+	year: PropTypes.any,
+	title: PropTypes.string,
+	duration: PropTypes.array,
+	details: PropTypes.string,
 };
 
 export default WorkItem;
